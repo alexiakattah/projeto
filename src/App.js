7d24359cfe8c5b64f7e41841fd5bb40072ec2785
Alexia Kattah <alexiakattah_97@hotmail.com>
2019-06-06 21:29:51 -0300

    Criação do componente de mapa

--- src/App.js
+++ src/App.js
@@ -1,69 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-// import {MapView} from 'react-native-maps';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import styled from 'styled-components/native'
+import Map from './components/Map'
+import { Text, View } from 'react-native';
 
-export default () => (
-  <View style={styles.container}>  
+const RootView = styled(View)`
+  background-color: white;
+  flex: 1;
+`
 
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }} 
-    >
-    
-    </MapView>
-  </View>
-);
-
-//  export default class App extends React.Component {
-//   render() {
-//     return (
-        
-//       <MapView
-//         style={styles.map}
-//         loadingEnabled={true}
-//         region={{
-//         latitude: 37.78825,
-//         longitude: -122.4324,
-//         latitudeDelta: 0.015,
-//         longitudeDelta: 0.0121,
-//         }}
-//       >
-//       </MapView>     
-//     );
-//   }
-//}
-
-const styles = StyleSheet.create({
-  container: {
-     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-
-    position: 'absolute',
-    top: 70,
-    left: 10,
-    right: 10,
-    bottom: 200,
-  },
-  header:{
-    
+export default class App extends React.Component {
+  render() {
+    return (
+      <RootView>
+        <Text>Header</Text>
+        <Map />
+      </RootView>
+    );
   }
-});
+}
---
+++ src/components/Map.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const RootView = styled(View)`
+  background-color: blue;
+  height: 200;
+`
+
+const CustomMapView = styled(MapView)`
+  flex: 1;
+`
+
+export default class Map extends React.Component {
+  render() {
+    return (
+      <RootView>
+        <CustomMapView
+          loadingEnabled={true}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </CustomMapView>
+      </RootView>
+    );
+  }
+}
