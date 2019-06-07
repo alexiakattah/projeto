ec7f9f918f2a08a9f9fe43e92cb6795afe4b11df
Alexia Kattah <alexiakattah_97@hotmail.com>
2019-06-07 00:47:55 -0300

    Adiciona pontos no mapa

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import { View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import { buscarDadosMapa } from './../data/Api' 
 
 const RootView = styled(View)`
   height: 240;
@@ -13,20 +15,36 @@ const CustomMapView = styled(MapView)`
 `
 
 export default class Map extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { items: [], isLoading: false }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+
+    buscarDadosMapa()
+      .then((items) => { 
+        this.setState({ items: items, isLoading: false }) 
+      })
+      .catch((error) => { 
+        this.setState({ items: [], isLoading: false }) 
+      })
+  }
+
   render() {
+    const region = {latitude: -20.1411, longitude: -44.8916, latitudeDelta: 0.015, longitudeDelta: 0.0121}
+
+    const markers = this.state.items.map(item => (
+      <Marker
+        key={item.id}
+        coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+      />
+    ))
+
     return (
       <RootView>
-        <CustomMapView
-          loadingEnabled={true}
-          provider={PROVIDER_GOOGLE}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </CustomMapView>
+        <CustomMapView initialRegion={region} loadingEnabled={true} provider={PROVIDER_GOOGLE}>{markers}</CustomMapView>
       </RootView>
     );
   }
